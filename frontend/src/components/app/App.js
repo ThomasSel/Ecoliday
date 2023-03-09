bdc5d8ccac96f0a768bd74f6980d3224ca20e830
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-03-09 14:01:18 +0000

    SQUASH - Add a button to test navigate on /test

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -11,7 +11,24 @@ const App = () => {
       <Route path="/signup" element={<SignUpForm navigate={useNavigate()} />} />
       <Route path="/login" element={<LoginForm navigate={useNavigate()} />} />
       <Route path="/trips" element={<UserTrips navigate={useNavigate()} />} />
+      <Route path="/test" element={<Test navigate={useNavigate()} />} />
     </Routes>
+  );
+};
+
+const Test = ({ navigate }) => {
+  return (
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        console.log("Calling navigate('/')");
+        navigate("/");
+        console.log("navigate('/') has been called");
+      }}
+      className="w-20 h-20 bg-white"
+    >
+      Click me!
+    </button>
   );
 };
 
